0deebfd21c197a69ce389a5b6b26517f6346a068
Emik123dsa <pengrobinson@mail.ru>
2020-01-10 00:53:24 +0500

    Something occured :(

--- js/main.js
+++ js/main.js
@@ -1,15 +1,24 @@
 
 (function() {
-   
+
+const parallax_feature = document.getElementById('parallax_first');
+const menuMain = document.querySelector('.navbar_main');
 
 function scroll () {
     if (pageYOffset >= 100) {
+        menuMain.classList.add('navbar_main_active');
+        console.log(pageYOffset-1224);
+        parallax_feature.style.backgroundPositionY = `${pageYOffset*0.5-900}px`;
         
     }
     else {
-      
+        menuMain.classList.remove('navbar_main_active');
     }
 }
+
+
+
+
 window.addEventListener('scroll', scroll);
 
 
