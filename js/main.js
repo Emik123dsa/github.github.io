3a2b441770c3c144cd92f10c4a1e724f2fd1cd9c
Emik123dsa <pengrobinson@mail.ru>
2020-01-11 02:02:10 +0500

    Almost finished?

--- js/main.js
+++ js/main.js
@@ -4,12 +4,110 @@
 const parallax_feature = document.getElementById('parallax_first');
 const menuMain = document.querySelector('.navbar_main');
 
+const button_slick_first = document.getElementById('slick-slide-control00');
+const button_slick_second = document.getElementById('slick-slide-control01');
+const button_slick_third = document.getElementById('slick-slide-control02');
+
+const animate_first = document.querySelector('.hero_wrap_dentist_equipments_first_description_title');
+const button_first = document.querySelector('.hero_wrap_dentist_equipments_first_button');
+const span_first = document.querySelector('.hero_wrap_dentist_equipments_first_description_feature');
+const image_first = document.querySelector('.hero_wrap_dentist_equipments_first_feature');
+
+const animate_second = document.querySelector('.hero_wrap_dentist_equipments_second_description_title');
+const button_second = document.querySelector('.hero_wrap_dentist_equipments_second_button');
+const span_second = document.querySelector('.hero_wrap_dentist_equipments_second_description_feature');
+const image_second = document.querySelector('.hero_wrap_dentist_equipments_second_feature');
+
+const animate_third = document.querySelector('.hero_wrap_dentist_equipments_third_description_title');
+const button_third = document.querySelector('.hero_wrap_dentist_equipments_third_button');
+const span_third = document.querySelector('.hero_wrap_dentist_equipments_third_description_feature');
+const image_third = document.querySelector('.hero_wrap_dentist_equipments_third_feature');
+
+button_slick_first.addEventListener('click', function() {
+    animate_first.classList.add('animated', 'fadeInLeftBig');
+    button_first.classList.add('animated', 'fadeInRightBig');
+    span_first.classList.add('animated', 'fadeInDownBig');
+    image_first.classList.add('animated', 'fadeInDownBig');
+
+    //
+
+    animate_second.classList.remove('animated', 'fadeInLeftBig');
+    button_second.classList.remove('animated', 'fadeInRightBig');
+    span_second.classList.remove('animated', 'fadeInDownBig');
+    image_second.classList.remove('animated', 'fadeInDownBig');
+
+    //
+
+    animate_third.classList.remove('animated', 'fadeInLeftBig');
+    button_third.classList.remove('animated', 'fadeInRightBig');
+    span_third.classList.remove('animated', 'fadeInDownBig');
+    image_third.classList.remove('animated', 'fadeInDownBig')
+});
+
+button_slick_second.addEventListener('click', function() {
+    animate_first.classList.remove('animated', 'fadeInLeftBig');
+    button_first.classList.remove('animated', 'fadeInRightBig');
+    span_first.classList.remove('animated', 'fadeInDownBig');
+    image_first.classList.remove('animated', 'fadeInDownBig');
+
+    //
+
+    animate_second.classList.add('animated', 'fadeInLeftBig');
+    button_second.classList.add('animated', 'fadeInRightBig');
+    span_second.classList.add('animated', 'fadeInDownBig');
+    image_second.classList.add('animated', 'fadeInDownBig');
+
+    //
+
+    animate_third.classList.remove('animated', 'fadeInLeftBig');
+    button_third.classList.remove('animated', 'fadeInRightBig');
+    span_third.classList.remove('animated', 'fadeInDownBig');
+    image_third.classList.remove('animated', 'fadeInDownBig')
+});
+
+button_slick_third.addEventListener('click', function() {
+    animate_first.classList.remove('animated', 'fadeInLeftBig');
+    button_first.classList.remove('animated', 'fadeInRightBig');
+    span_first.classList.remove('animated', 'fadeInDownBig');
+    image_first.classList.remove('animated', 'fadeInDownBig');
+
+    //
+
+    animate_second.classList.remove('animated', 'fadeInLeftBig');
+    button_second.classList.remove('animated', 'fadeInRightBig');
+    span_second.classList.remove('animated', 'fadeInDownBig');
+    image_second.classList.remove('animated', 'fadeInDownBig');
+
+    //
+
+    animate_third.classList.add('animated', 'fadeInLeftBig');
+    button_third.classList.add('animated', 'fadeInRightBig');
+    span_third.classList.add('animated', 'fadeInDownBig');
+    image_third.classList.add('animated', 'fadeInDownBig')
+});
+
+button_slick_second.addEventListener('click', function() {
+    
+});
+
+button_slick_third.addEventListener('click', function() {
+    
+});
+
+const forms_appear = document.querySelector('.forms_features_wrap_users'); 
+
+window.addEventListener("DOMContentLoaded", function() {
+    animate_first.classList.add('animated', 'fadeInLeftBig');
+    button_first.classList.add('animated', 'fadeInRightBig');
+    span_first.classList.add('animated', 'fadeInDownBig');
+    image_first.classList.add('animated', 'fadeInDownBig');
+});
+
+
 function scroll () {
     if (pageYOffset >= 100) {
         menuMain.classList.add('navbar_main_active');
-        console.log(pageYOffset-1224);
-        parallax_feature.style.backgroundPositionY = `${pageYOffset*0.5-900}px`;
-        
+       parallax_feature.style.backgroundPositionY = `${pageYOffset/2-950}px`;
     }
     else {
         menuMain.classList.remove('navbar_main_active');
--- js/slick-features.js
+++ js/slick-features.js
@@ -2,8 +2,12 @@ $('.hero_wrap_dentist_equipments').slick({
     infinite: true,
 	dots: true,
 	arrows: false,
-	autoplay: true,
-	autoplaySpeed: 5000,
 	fade: true,
 	fadeSpeed: 2000
+});
+
+$('.offers_features_background_description').slick({
+	infinite: true,
+	fade: true,
+	fadeSpeed: 4000
 });
